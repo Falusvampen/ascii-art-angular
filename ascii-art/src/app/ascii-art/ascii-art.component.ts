c3e6e2df3e509a69b6eaf1aca9f16abdc86f515b
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-10-29 15:49:03 +0200

    added newline support and removed bad font

--- ascii-art/src/app/ascii-art/ascii-art.component.ts
+++ ascii-art/src/app/ascii-art/ascii-art.component.ts
@@ -9,51 +9,85 @@ import { HttpClient } from '@angular/common/http';
 export class AsciiArtComponent implements OnInit {
   public inputText: string = '';
   public asciiArtText: string = '';
+  public selectedFont: string = 'standard';
   private asciiMap: { [key: string]: string } = {};
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.readAndParseFontFile('assets/Fonts/standard.txt').then(map => {
+    this.readAndParseFontFile(this.selectedFont).then(map => {
       this.asciiMap = map;
     });
   }
 
-  async readAndParseFontFile(filename: string) {
-  try {
-    const content = await this.http.get(filename, { responseType: 'text' }).toPromise();
-    if (content) {
-      const asciiChars = content.split('\n\n');
-      const asciiMap: { [key: string]: string } = {};
-      let asciiIndex = 32;
-
-      for (const asciiArt of asciiChars) {
-        asciiMap[String.fromCharCode(asciiIndex)] = asciiArt;
-        asciiIndex++;
+  async readAndParseFontFile(font: string) {
+    const filename = `assets/Fonts/${font}.txt`;
+    try {
+      const content = await this.http.get(filename, { responseType: 'text' }).toPromise();
+      if (content) {
+        const asciiChars = content.split('\n\n');
+        const asciiMap: { [key: string]: string } = {};
+        let asciiIndex = 32;
+        for (const asciiArt of asciiChars) {
+          asciiMap[String.fromCharCode(asciiIndex)] = asciiArt;
+          asciiIndex++;
+        }
+        return asciiMap;
+      } else {
+        console.error('File content is undefined');
+        return {};
       }
-      return asciiMap;
-    } else {
-      console.error('File content is undefined');
+    } catch (error) {
+      console.error('An error occurred while reading the file:', error);
       return {};
     }
-  } catch (error) {
-    console.error('An error occurred while reading the file:', error);
-    return {};
   }
+
+  async changeFont() {
+    this.readAndParseFontFile(this.selectedFont).then(map => {
+      this.asciiMap = map;
+      this.textToAsciiArt();
+    });
+  }
+
+  textToAsciiArt() {
+  // Initialize an array to hold lines of ASCII art
+  let asciiArtLines: string[] = [];
+  
+  // Split the input text into lines
+  const inputLines = this.inputText.split('\n');
+  
+  // Process each line individually
+  for (const line of inputLines) {
+    // Initialize an array to hold the ASCII art for the current line
+    const linesForCurrentInput = new Array(8).fill('');
+  
+    // Process each character in the line
+    for (const char of line) {
+      let charArt: string[];
+      if (char === ' ') {
+        charArt = new Array(8).fill('        ');
+      } else if (this.asciiMap[char]) {
+        charArt = this.asciiMap[char].split('\n');
+      } else {
+        charArt = new Array(8).fill('????????');
+      }
+  
+      charArt.forEach((artLine, index) => {
+        linesForCurrentInput[index] += artLine;
+      });
+    }
+  
+    // Combine the lines of ASCII art for the current line into a single string
+    const combinedLinesForCurrentInput = linesForCurrentInput.join('\n');
+  
+    // Add the ASCII art for the current line to the overall array
+    asciiArtLines.push(combinedLinesForCurrentInput);
+  }
+  
+  // Join all the lines of ASCII art, separating them with extra newlines
+  this.asciiArtText = asciiArtLines.join('\n\n');
 }
 
 
-  textToAsciiArt() {
-    const lines = new Array(8).fill('');
-    for (const char of this.inputText) {
-
-      if (!this.asciiMap[char]) continue;
-      
-      const charArt = this.asciiMap[char].split('\n');
-      charArt.forEach((line, index) => {
-        lines[index] += line + '';
-      });
-    }
-    this.asciiArtText = lines.join('\n');
-  }
 }
